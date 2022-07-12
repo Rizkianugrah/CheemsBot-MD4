79542acfdae38b6c1cafdfa51e44ad119f2a33b5
Rizkianugrah <46855274+Rizkianugrah@users.noreply.github.com>
2022-07-12 11:30:53 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -56,7 +56,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 //owmner v card
 global.vcardowner = ['6281387089252'] //ur owner number
 global.ownername = "🦄𝓜𝓪𝓾𝓵𝓪𝓷𝓪 𝓨𝓾𝓼𝓾𝓯" //ur owner name
-global.ytname = "Maulana Yusuf" //ur yt chanel name
+global.fbname = "Maulana Yusuf" //ur Facebook name
 global.socialm = "https://194.233.66.232/" //ur github or insta name
 global.location = "Indonesia, Jawa Barat, Purwakarta" //ur location
 
